ba2b324e99333f159d5091e701ded27ef1942e96
Maciej Pałubicki <palubickimaciej5@gmail.com>
2023-04-28 00:36:11 +0200

    Add project files.

---
+++ FileSorter/Program.cs
@@ -0,0 +1,23 @@
+﻿using FileSorter;
+using SortFile;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CustomFileSort customFileSort = new CustomFileSort();
+        string path = "c:\\Users\\Reszt\\Desktop";
+
+        List<CustomFileType> files = new List<CustomFileType>();
+
+        CustomFileType fileType = new CustomFileType()
+        {
+            DirectoryName = "Zdjecia",
+            Extenstions = new List<string>() {".jpg",".png"}
+        };
+
+        files.Add(fileType);
+
+        customFileSort.Sort(path,files);
+    }
+}
---
+++ SortFile/FileSort.cs
@@ -0,0 +1,36 @@
+﻿using FileSorter;
+
+namespace SortFile
+{
+    public class CustomFileSort
+    {
+        public void Sort(string path,List<CustomFileType> fileTypes)
+        {
+            foreach (CustomFileType fileType in fileTypes)
+            {
+                string directoryPath = $"{path}{Path.DirectorySeparatorChar}{fileType.DirectoryName}";
+                
+                if (!Directory.Exists(directoryPath)) 
+                {
+                   Directory.CreateDirectory(directoryPath);
+                }
+
+                List<string> files = Directory.GetFiles(path).ToList();
+
+                foreach (string file in files)
+                {
+                    string fileWithoutExt = Path.GetExtension(file);
+
+                    foreach(string ext in fileType.Extenstions)
+                    {
+                        if(fileWithoutExt ==  ext)
+                        {
+                            File.Move(file, $"{directoryPath}{Path.DirectorySeparatorChar}{Path.GetFileName(file)}");
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
